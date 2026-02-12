c83471539843cf6d9554aefaf021c5ed14b4206c
YanamalaVenu <venureddy191@gmail.com>
2026-02-12 21:21:25 +0530

    commit

---
+++ Day1_Variable/Day3-operators_Loops/ex.py
@@ -0,0 +1,76 @@
+#operators
+'''
+1. Arithmetic Operators-(+ - * / % // **)
+2. Comparison (Relational) Operators-(== != > < >= <=)
+3. Logical Operators-(and or not)
+4. Assignment Operators-(= += -= *= /=)
+5. Membership Operators-(in not in)
+6. Identity Operators-(is is not)
+'''
+#Loops
+'''
+Loop is used to excute a block of code multiple times
+
+Types
+1.for
+2.while
+
+Loop comtrol
+1.break
+2.control
+3.pass
+'''
+# for loop
+#Example 1: Print numbers 1 to 5
+# for i in range(1,6):
+#     print(i)
+
+# fruits=["apple","banana","mango"]
+# for x in fruits: 
+#     print(x)
+
+# total=1
+# for i in range(1,6):
+#     total=total+i
+# print(total)
+
+#while loop
+#print 1 to 5
+# i=1
+# while i<=5:
+#     print(i)
+#     i=i+1
+
+# password = ""
+
+# while password != "admin123":
+#     password = input("Enter password: ")
+
+# print("Login successful")
+
+# for i in range(1,11):
+#     if i%2 ==0:
+#         print(i)
+
+#add 2 numbers
+# a=1
+# b=2
+# c=a+b
+# print(c)
+
+#swapping of 2 numbers
+# a=10
+# b=20
+# c=a
+# a=b
+# b=c
+# print(a,b)
+
+#check even or  add
+# a=int(input("enter number"))
+# if a%2==0:
+#     print(a,"is even")
+# else:
+#     print(a, "is odd")
+
+
